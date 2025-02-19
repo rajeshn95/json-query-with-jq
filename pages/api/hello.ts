c0c1866f1d67ab9ffd76c8f10be1577343a44768
rajeshn95 <rajesh@betalectic.com>
2025-02-19 12:33:38 +0530

    get route edge runtime

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,9 +5,13 @@ type Data = {
   name: string;
 };
 
+export const config = {
+  runtime: "edge",
+};
+
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
   res.status(200).json({ name: "John Doe" });
 }
