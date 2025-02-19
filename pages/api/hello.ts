57c250d2348662ebd2c391a6fffac0c9c8451434
rajeshn95 <rajesh@betalectic.com>
2025-02-19 12:46:38 +0530

    changes for the edge runtime

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,18 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-
-type Data = {
-  name: string;
-};
+// import type { NextApiRequest, NextApiResponse } from "next";
 
 export const config = {
   runtime: "edge",
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default function handler() {
   const response = new Response(JSON.stringify({ name: "John Doe" }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
--- pages/api/jq-query-edge.ts
+++ pages/api/jq-query-edge.ts
@@ -1,5 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
 // const jq = require("jq-wasm");
 import * as jq from "jq-wasm";
 
@@ -9,12 +8,10 @@ export const config = {
   runtime: "edge",
 };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log("EDGE-RUNTIME Request--->", req.body, req.body.data);
-  console.log("EDGE-RUNTIME Data--->", typeof req.body.data, req.body.data);
+export default async function handler(req: Request) {
+  const body = await req.json();
+  console.log("EDGE-RUNTIME Request--->", body, body.data);
+  console.log("EDGE-RUNTIME Data--->", typeof body.data, body.data);
 
   // Using jq.raw for raw text output
   //   const rawResult = await jq.raw(req.body.data, ".foo", ["-c"]);
@@ -22,16 +19,18 @@ export default async function handler(
 
   if (!jq) {
     console.error("EDGE-RUNTIME jq is not initialized");
-    res.status(500).json({ error: "jq is not initialized" });
-    return;
+    return new Response(JSON.stringify({ error: "jq is not initialized" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 
   // Using jq.json for parsed JSON output
-  const result: JqResult = await jq.json(
-    JSON.parse(req.body.data),
-    req.body.query
-  );
+  const result: JqResult = await jq.json(JSON.parse(body.data), body.query);
   console.log("EDGE-RUNTIME Result--->", result); // Output: ["bar"]
 
-  res.status(200).json(result);
+  return new Response(JSON.stringify(result), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 }
