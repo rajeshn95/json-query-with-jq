ee0ae7707f40f5c9dae9e81863c31507078090ee
rajeshn95 <rajesh@betalectic.com>
2025-02-19 10:43:59 +0530

    Fix: Jq import

--- pages/api/jq-query.ts
+++ pages/api/jq-query.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 // const jq = require("jq-wasm");
-import jq from "jq-wasm";
+import * as jq from "jq-wasm";
 
 type JqResult = object | object[] | null;
 
